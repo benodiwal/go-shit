4b534287bcc052722fbee427331335fda1484ff8
Sachin <s474996633@gmail.com>
2024-07-18 19:54:37 +0530

    version command

---
+++ pkg/cli/version.go
@@ -0,0 +1,20 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command {
+	Use: "version",
+	Short: "Print the version number of hugo",
+	Long:  `All software has versions. This is Hugo's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
+	},
+}
